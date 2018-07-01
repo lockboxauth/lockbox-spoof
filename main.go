1c477ca861f51493b1950580e7861a961e327039
Paddy Carver <paddy@impractical.co>
2018-06-30 22:35:40 -0700

    First commit.
    Simple command line tool for spoofing a JWT session, if you know the JWT
    secret.

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"impractical.co/auth/sessions"
+)
+
+func main() {
+	key := os.Getenv("JWT_KEY")
+	if key == "" {
+		fmt.Println("JWT_KEY must be specified")
+		os.Exit(1)
+	}
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./spoof PROFILE_ID")
+		os.Exit(1)
+	}
+	profile := os.Args[1]
+	deps := sessions.Dependencies{
+		JWTPrivateKey: key,
+		JWTPublicKey:  key,
+	}
+	token := sessions.AccessToken{
+		ID:          "spoof",
+		CreatedFrom: "spoof",
+		ProfileID:   profile,
+		ClientID:    "spoof",
+		CreatedAt:   time.Now(),
+	}
+	jwt, err := deps.CreateJWT(context.Background(), token)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(jwt)
+}
